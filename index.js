705987f3e436c94fcf7ef35a22d6e41d39918cee
Jimmy MOSTOVOI <jm@vyzyo.com>
2023-12-08 04:59:24 +0100

    export

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 import { addClass, hasClass, removeClass } from "./helpers/cssclasses";
 import { getParentsList, findParent } from "./helpers/elparents";
+//
+export { addClass, hasClass, removeClass, getParentsList, findParent } 
